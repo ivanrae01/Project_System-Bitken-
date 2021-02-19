7304179c3766ca4589123749f77849d5375d7327
Trisha Anne N. Dellava <72644923+Trisha-Anne-Dellava@users.noreply.github.com>
2021-02-19 17:34:55 +0800

    Add files via upload

---
+++ home.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+ <title></title>
+ <link rel="preconnect" href="https://fonts.gstatic.com">
+ <link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Indie+Flower&display=swap" rel="stylesheet">
+ <link rel="stylesheet" type="text/css" href="style.css">
+<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+</head>
+<body> 
+
+<header class="site-header clearfix">
+ <nav>
+  <div class="logo">
+   <h1>Modern Website</h1>
+  </div>
+  <div class="menu"> 
+   <ul>
+    <li>Home <a href=""></a></li>
+    <li>User</li>
+    <li>Rankings</li>
+    <li>Contact Us</li>
+   </ul>
+  </div>
+ </nav>
+ <section>
+  <div class="leftside"> 
+   <img src="girl1.jpg">
+  </div>
+  <div class="rightside"> 
+   <h1>BITKEN</h1>
+   <p>  Study Aesthetically with us! </p>
+   <button>Welcome</button>
+  </div>
+  
+ </section>
+
+</header>
+
+
+</body>
+</html>
